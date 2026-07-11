5c61ba68998624bc120808352e85071592253397
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-07-11 15:44:59 +0530

    Create 02.LongestSubstringWithoutRepeatingCharacters.java

---
+++ 03. Sliding Window/02.LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+      Map<Character,Integer> map=new HashMap<>();
+      int l=0,ans=0;
+      for(int r=0;r<s.length();r++){
+         char c=s.charAt(r);
+         if(map.containsKey(c)) l=Math.max(l,map.get(c)+1);
+         map.put(c,r);
+         ans=Math.max(ans,r-l+1);
+      }  
+      return ans;
+    }
+}
